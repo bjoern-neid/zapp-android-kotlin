94d6d3ebb99d383ab439b8623a45abcf1412e428
Andrej <andrej.wolfenhaut@st.oth-regensburg.de>
2022-04-26 08:40:26 +0200

    Changed a random letter to C to create conflict >:)

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
@@ -1,25 +1,25 @@
 package com.example.othregensburg.zapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    // TODO (4) Create a new class for the adapter which extends from RecyclerView.Adapter<ListAdapter.ViewHolder>
+
     private val randomValues =
-        List(90) { ('B' + Random.nextInt('Z' - 'A')).toString() + " " + it.toString() }
+        List(50) { ('C' + Random.nextInt('Z' - 'A')).toString() + " " + it.toString() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        // TODO (5) Find the RecyclerView defined in the layout and create a reference
 
+        // TODO (6) Set a LayoutManager (LinearLayoutManager) to the RecyclerView
 
-        recyclerView.adapter = ListAdapter(randomValues)
+        // TODO (7) Set the newly created custom RecyclerView.Adapter to the RecyclerView
+
     }
 }
